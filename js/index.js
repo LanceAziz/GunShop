f42de051785f8188d731a7a47423f4c56f63fdd6
LanceAziz <lanceaziz2002@gmail.com>
2023-03-21 00:14:04 +0200

    API Testing

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// Nav Bar
 document.getElementById("nav-placeholder").innerHTML = `
 <div class="container-fluid">
         <!-- Logo -->
@@ -23,3 +24,43 @@ document.getElementById("nav-placeholder").innerHTML = `
             <i class="fa-solid fa-user-plus px-2 py-2 fs-5 rounded-0 skew"></i>
         </div>
     </div>`;
+var myHttp = new XMLHttpRequest();
+var myMovies = [];
+myHttp.open('GET', 'https://api.themoviedb.org/3/trending/movie/week?api_key=713e5b505cc52f5aa68161a6f9c471c1');
+myHttp.send();
+myHttp.addEventListener('readystatechange', function () {
+    if (myHttp.status == 200 && myHttp.readyState == 4) {
+        myMovies = JSON.parse(myHttp.response).results;
+        console.log(myMovies);
+        diaplayMovies()
+    }
+});
+function diaplayMovies() {
+    var temp = ``;
+    for (let i = 0; i < myMovies.length; i++) {
+        temp += `
+            <div class="col-3 py-2">
+                <div class="text-center border-grad">
+                    <!-- Picture -->
+                    <div class="text-center p-3">
+                        <img class="w-100" src="https://image.tmdb.org/t/p/w500/${myMovies[i].poster_path}">
+                    </div>
+                    <!-- Details -->
+                    <div class="my-2">
+                        <h3>${myMovies[i].title}</h3>
+                        <h4>${myMovies[i].vote_average}</h4>
+                    </div>
+                    <!-- Buttons -->
+                    <div class="d-flex justify-content-center my-4">
+                        <button type="button" class="btn btn-primary px-4 py-2 fs-6 rounded-0 skew"><i
+                                class="fa-solid fa-cart-plus"></i></button>
+                        <button type="button" class="btn btn-primary px-4 py-2 fs-6 rounded-0 skew"><i
+                                class="fa-solid fa-cart-arrow-down"></i></button>
+                        <button type="button" class="btn btn-primary px-4 py-2 fs-6 rounded-0 skew"><i
+                                class="fa-solid fa-circle-info"></i></button>
+                    </div>
+                </div>
+            </div>`
+    }
+    document.getElementById('products').innerHTML = temp;
+}
