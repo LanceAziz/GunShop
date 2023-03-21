e722b7677f589a69bceb523cc91ac87b2b4f4446
LanceAziz <lanceaziz2002@gmail.com>
2023-03-22 01:18:39 +0200

    Small Teeks V.6

--- js/index.js
+++ js/index.js
@@ -2,27 +2,33 @@
 document.getElementById("nav-placeholder").innerHTML = `
 <div class="container-fluid">
         <!-- Logo -->
-        <div class="navbar-nav me-2">
-            <a href="index.html"><i class="fa-solid fa-gun px-2 py-2 fs-5 rounded-0 skew"></i></a>
+        <div class="col-4">
+            <div class="navbar-nav">
+                <a href="index.html"><i class="fa-solid fa-person-rifle px-2 py-2 fs-5 rounded-0 skew"></i></a>
+            </div>
         </div>
         <!-- Navigation Tabs -->
-        <ul class="navbar-nav m-auto">
-            <li class="nav-item">
-                <a class="nav-link text-white" aria-current="page" href="index.html #Home">Home</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link text-white" href="index.html #About-Us">About</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link text-white" href="index.html #Categories">Categories</a>
-            </li>
-        </ul>
-        <!-- Cart & Athuntication -->
-        <div class="navbar-nav me-2">
-            <i class="fa-solid fa-cart-shopping px-2 py-2 fs-5 rounded-0 skew"></i>
-            <i class="fa-solid fa-right-to-bracket px-2 py-2 fs-5 rounded-0 skew"></i>
-            <i class="fa-solid fa-user-plus px-2 py-2 fs-5 rounded-0 skew"></i>
+        <div class="col-4">
+            <ul class="navbar-nav m-auto">
+                <li class="nav-item">
+                    <a class="nav-link text-white" aria-current="page" href="index.html #Home">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="index.html #About-Us">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="index.html #Categories">Categories</a>
+                </li>
+            </ul>
         </div>
+        <!-- Cart & Athuntication -->
+        <div class="col-4">
+            <div class="navbar-nav">
+                <i class="fa-solid fa-cart-shopping px-2 py-2 fs-5 rounded-0 skew"></i>
+                <i class="fa-solid fa-right-to-bracket px-2 py-2 fs-5 rounded-0 skew"></i>
+                <i class="fa-solid fa-user-plus px-2 py-2 fs-5 rounded-0 skew"></i>
+             </div>
+        </div>    
     </div>`;
 // API
 (async function getProducts(){
