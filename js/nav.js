5503c0bbc9a40f40cf7f8e013ec27dbeca2c1266
LanceAziz <lanceaziz2002@gmail.com>
2023-03-26 12:02:46 +0200

    nav bar in component & responsive about us

--- js/nav.js
+++ js/nav.js
@@ -29,7 +29,52 @@ document.getElementById("nav-placeholder").innerHTML = `
                 </div>
             </div>
         </div>
-    </nav>`;
+    </nav>
+    <!-- Cart Items -->
+    <div id="cart-menu" class="crt-menu d-flex flex-column justify-content-between">
+        <div class="sticky-md-top">
+            <!-- Header -->
+            <div class="d-flex justify-content-between bb-crt-item py-2">
+                <h2>Cart Items</h2>
+                <i class="fa-solid fa-xmark trash-ani fs-3 p-2 px-3"></i>
+            </div>
+            <!-- items -->
+            <div class="scroll">
+                <div class="row mt-4">
+                    <div class="col-md-2">
+                        <img class="w-100" src="Images/demo.jpg" alt="">
+                    </div>
+                    <div class="col-md-8 p-2">
+                        <h2 class="fs-6" id="name">Name</h2>
+                        <h2 class="fs-6">QTY: <span id="Qty">2</span></h2>
+                        <h2 class="fs-6" id="price">$2000.00</h2>
+                    </div>
+                    <div class="col-md-2 d-flex justify-content-center align-items-center">
+                        <i class="fa-solid fa-trash trash-ani fs-5"></i>
+                    </div>
+                </div>
+            </div>
+            <!-- Checkout -->
+            <div class="sticky-md-bottom bt-crt-item bg-black pt-3">
+                <div class="d-flex justify-content-between">
+                    <h3>Subtotal:</h3>
+                    <h3 id="subtotal">$4000.00</h3>
+                </div>
+                <div class="row g-2">
+                    <div class="col-md-6">
+                        <button
+                            class="btn border-start-0 border-end-0 rounded-0 btn-primary fa-sm p-4 w-100 my-1">Clear
+                            Cart</button>
+                    </div>
+                    <div class="col-md-6">
+                        <button
+                            class="btn border-start-0 border-end-0 rounded-0 btn-primary fa-sm p-4 w-100 my-1">Check
+                            Out</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>`;
     
 let dWidth = $(window).width();
 $('#cart-menu').css('left', `${dWidth}px`);
